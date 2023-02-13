433cc490e19221f007a6d1af19408a10451e3bee
Rémi Bastide <Remi.Bastide@irit.fr>
2023-02-13 16:11:25 +0100

    heroku

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <ul>
-      <li><a href="http://localhost:8989/swagger-ui/index.html" target="_blank">Swagger</a></li>
-      <li><a href="http://localhost:8989/h2-console" target="_blank">Console H2 (URL jdbc:h2:mem:testdb)</a></li>
+      <li><a href="https://monorepo.herokuapp.com/swagger-ui/index.html" target="_blank">Swagger</a></li>
+      <li><a href="https://monorepo.herokuapp.com/h2-console" target="_blank">Console H2 (URL jdbc:h2:mem:testdb)</a></li>
     </ul>
   </div>
 </template>
