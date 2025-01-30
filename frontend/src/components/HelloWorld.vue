549492139b4af27513471910d2fb0d22827214ba
Rémi Bastide <Remi.Bastide@irit.fr>
2025-01-30 16:34:17 +0100

    versions node et java

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container-fluid">
     <ul>
-      <li><a href="/swagger-ui/index.html" target="_blank">Swagger</a></li>
+      <li><a href="/swagger-ui/index.html" target="_blank">Swagger XXX</a></li>
       <li><a href="/h2-console" target="_blank">Console H2 (URL jdbc:h2:mem:testdb)</a></li>
     </ul>
   </div>
