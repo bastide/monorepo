ffd6874b5f61e22db034621669b7f286ba171e85
Rémi Bastide <Remi.Bastide@irit.fr>
2025-02-02 18:19:51 +0100

    versions node et java

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -16,6 +20,7 @@ export default defineConfig({
     outDir: 'target/dist',
     assetsDir: 'static',
   },
+  // Utile uniquement pour le serveur de développement qui tourne sur un autre port que le backend
   server: {
     proxy: { // On redirige toutes les requêtes au backend vers le serveur de développement java
       '/api': { // L'API REST autogénérée, correspond à la config du backend spring.data.rest.base-path dans application.properties
